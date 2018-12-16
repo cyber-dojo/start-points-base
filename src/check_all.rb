f3e59143c07b975c6b00f53e2a64a9b9ccf234b7
Jon Jagger <jon@jaggersoft.com>
2018-12-16 21:14:42 +0000

    get shas from file put into context-dir by script

--- src/check_all.rb
+++ src/check_all.rb
@@ -1,8 +1,14 @@
 
 def shas_filename
-  '/app/shas.txt'
+  '/app/repos/shas.txt'
 end
 
 def root_dir
   ARGV[0]
 end
+
+puts "root_dir==#{root_dir}"
+puts `ls -al #{root_dir}`
+
+puts "shas"
+puts `cat #{root_dir}/shas.txt`
