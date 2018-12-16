47ac5d07313b4b0d4f72a62d7a55c64798ccc19d
Jon Jagger <jon@jaggersoft.com>
2018-12-16 19:23:48 +0000

    pass repo names via stdin

--- src/clone_all.rb
+++ src/clone_all.rb
@@ -1,7 +1,11 @@
 
-repo_names = IO.read(ARGV[0]).split.map!(&:strip)
+def repo_names
+  gets.chomp.split(/ /)
+end
+
 repo_names.each_with_index do |repo_name, index|
   name = repo_name.split('/')[-1]
+  puts ":#{name}:"
   `git clone --depth 1 #{repo_name} #{index}/#{name}`
   # process it as per [cyber-dojo start-point create]
 end
