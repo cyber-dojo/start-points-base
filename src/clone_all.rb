93e566305a60acf9aa0e16143ce039b281bb7f87
Jon Jagger <jon@jaggersoft.com>
2018-12-16 18:20:58 +0000

    take name of file holding repo-names as command line arg

--- src/clone_all.rb
+++ src/clone_all.rb
@@ -1,5 +1,5 @@
 
-repo_names = IO.read('/app/repo-names').lines.map!(&:strip)
+repo_names = IO.read(ARGV[0]).lines.map!(&:strip)
 repo_names.each_with_index do |repo_name, index|
   name = repo_name.split('/')[-1]
   `git clone --depth 1 #{repo_name} #{index}/#{name}`
