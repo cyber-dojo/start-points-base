ba3b56150a15ed166acc2185943b645d3a38ec2d
Jon Jagger <jon@jaggersoft.com>
2018-12-17 07:15:50 +0000

    add argument checking to main script

--- build_cyber_dojo_start_point_image.sh
+++ build_cyber_dojo_start_point_image.sh
@@ -6,31 +6,24 @@ readonly CONTEXT_DIR=$(mktemp -d /tmp/start-points.XXXXXXXXX)
 readonly DOCKERFILE=${CONTEXT_DIR}/Dockerfile
 readonly DOCKER_IGNORE=${CONTEXT_DIR}/.dockerignore
 readonly IMAGE_NAME=${1}
-shift
-readonly REPO_NAMES=$#
+readonly REPO_NAMES=${@:2}
 
 # - - - - - - - - - - - - - - - - -
 
 show_use()
 {
-  echo "Use: ${MY_NAME} <image-name> [git-repo-url...]"
+  echo "use: ${MY_NAME} <image-name> <git-repo-urls>"
+  echo ""
+  echo "Create a cyber-dojo start-point image."
+  echo "Its base image will be cyberdojo/start-points-base"
+  echo "and it will contain clones of all the specific git repos."
 }
 
 # - - - - - - - - - - - - - - - - -
 
 exit_fail()
 {
-  >&2 echo "FAILED: $*"
   exit 1
-}
-
-# - - - - - - - - - - - - - - - - -
-
-check_docker_is_installed()
-{
-  if ! hash docker 2> /dev/null; then
-    exit_fail 'docker is not installed'
-  fi
 }
 
 # - - - - - - - - - - - - - - - - -
@@ -38,22 +31,48 @@ check_docker_is_installed()
 check_git_is_installed()
 {
   if ! hash git 2> /dev/null; then
-    exit_fail 'git is not installed'
+    echo 'git is not installed'
+    exit_fail
   fi
 }
 
 # - - - - - - - - - - - - - - - - -
-# todo: if no args or --help show_use
-# todo: check $1==image-name is provided
-# todo: check at least 1 repo-name is provided
-echo "IMAGE_NAME=:${IMAGE_NAME}:"
-echo "REPO_NAMES=:${REPO_NAMES}:"
 
+check_docker_is_installed()
+{
+  if ! hash docker 2> /dev/null; then
+    echo 'docker is not installed'
+    exit_fail
+  fi
+}
 
-# git clone all repos into docker context
-show_use
+# - - - - - - - - - - - - - - - - -
+
+check_args()
+{
+  if [ "${IMAGE_NAME}" = '--help' ];  then
+    show_use
+    exit_fail
+  fi
+  if [ "${IMAGE_NAME}" = '' ]; then
+    show_use
+    echo 'NO image_name'
+    exit_fail
+  fi
+  if [ "${REPO_NAMES}" = '' ]; then
+    show_use
+    echo 'NO git-repo-urls'
+    exit_fail
+  fi
+}
+
+# - - - - - - - - - - - - - - - - -
+
 check_git_is_installed
 check_docker_is_installed
+check_args
+
+# git clone all repos into docker context
 cd ${CONTEXT_DIR}
 for (( index=1; index<=$REPO_NAMES; index++ )); do
     declare repo_name=${!index}
@@ -77,3 +96,5 @@ docker build --tag ${IMAGE_NAME} ${CONTEXT_DIR}
 
 # tidy up
 rm -rf ${CONTEXT_DIR}
+
+show_use
