cea212fc1f3e473b660bc18e915f4437cf05a7af
Jon Jagger <jon@jaggersoft.com>
2018-12-22 12:24:20 +0000

    add circleci files

---
+++ .circleci/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly SCRIPT=build_cyber_dojo_start_points_image.sh
+readonly IMAGE_NAME=cyberdojo/start-points
+
+# build the FROM image so it won't be docker pulled
+docker build \
+  --tag cyberdojo/start-points-base \
+  ${MY_DIR}/..
+
+# smoke test building an image
+./${SCRIPT} \
+  ${IMAGE_NAME} \
+    https://github.com/cyber-dojo/start-points-languages.git \
+    https://github.com/cyber-dojo/start-points-exercises.git \
+    https://github.com/cyber-dojo/start-points-custom.git    \
