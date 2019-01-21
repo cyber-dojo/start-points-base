b6336dbfa58b412aed14d4f5c9183c31b9d4212d
Jon Jagger <jon@jaggersoft.com>
2019-01-21 11:41:59 +0000

    spike script now uses local git repo for exercises

--- .circleci/build_docker_image.sh
+++ .circleci/build_docker_image.sh
@@ -5,27 +5,33 @@ readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 readonly SCRIPT=build_cyber_dojo_start_points_image.sh
 readonly IMAGE_NAME=cyberdojo/start-points
 
-# create git repo from test-data
-readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-point-exercises.XXXXXXXXX)
-#cleanup() { rm -rf "${TMP_DIR}" > /dev/null; }
-#trap cleanup EXIT
-readonly TARGET_DIR="${ROOT_DIR}/test/data/exercises"
-cp -R "${TARGET_DIR}/" "${TMP_DIR}"
-cd "${TARGET_DIR}" && git init && git add . && git commit -m "initial commit"
-exit 1
+# - - - - - - - - - - - - - - - - -
+# create git repos from test-data
+readonly E_TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-point-exercises.XXXXXXXXX)
 
+cleanup()
+{
+  rm -rf "${E_TMP_DIR}" > /dev/null
+}
+trap cleanup EXIT
+
+readonly TARGET_DIR="${ROOT_DIR}/test/data/exercises"
+cp -R "${TARGET_DIR}/" "${E_TMP_DIR}"
+cd "${E_TMP_DIR}" \
+  && git init \
+  && git add . \
+  && git commit -m "initial commit" > /dev/null
+
+  # - - - - - - - - - - - - - - - - -
 # build the FROM image so it won't be docker pulled
 docker build \
   --tag cyberdojo/start-points-base \
   "${ROOT_DIR}"
 
+  # - - - - - - - - - - - - - - - - -
 # smoke test building an image
 "${ROOT_DIR}/${SCRIPT}" \
   ${IMAGE_NAME} \
+    file://${E_TMP_DIR} \
     https://github.com/cyber-dojo/start-points-languages.git \
     https://github.com/cyber-dojo/start-points-custom.git    \
-
-
-
-
-    #https://github.com/cyber-dojo/start-points-exercises.git \
