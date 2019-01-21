8e0d0ee2f0e3996cf13c0a6810c0ef11674f50f8
Jon Jagger <jon@jaggersoft.com>
2019-01-21 13:24:31 +0000

    start preparing server tests

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly SH_DIR="$( cd "$( dirname "${0}" )/sh" && pwd )"
 
-"${ROOT_DIR}/.circleci/build_docker_image.sh"
+"${SH_DIR}/build_docker_images.sh"
