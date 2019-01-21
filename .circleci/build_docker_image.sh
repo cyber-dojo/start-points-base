c0f21ad349f91878fb1d28a19097f86fe92fa7cd
Jon Jagger <jon@jaggersoft.com>
2019-01-21 11:31:20 +0000

    prepare exercises test data

--- .circleci/build_docker_image.sh
+++ .circleci/build_docker_image.sh
@@ -5,6 +5,15 @@ readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 readonly SCRIPT=build_cyber_dojo_start_points_image.sh
 readonly IMAGE_NAME=cyberdojo/start-points
 
+# create git repo from test-data
+readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-point-exercises.XXXXXXXXX)
+#cleanup() { rm -rf "${TMP_DIR}" > /dev/null; }
+#trap cleanup EXIT
+readonly TARGET_DIR="${ROOT_DIR}/test/data/exercises"
+cp -R "${TARGET_DIR}/" "${TMP_DIR}"
+cd "${TARGET_DIR}" && git init && git add . && git commit -m "initial commit"
+exit 1
+
 # build the FROM image so it won't be docker pulled
 docker build \
   --tag cyberdojo/start-points-base \
@@ -14,5 +23,9 @@ docker build \
 "${ROOT_DIR}/${SCRIPT}" \
   ${IMAGE_NAME} \
     https://github.com/cyber-dojo/start-points-languages.git \
-    https://github.com/cyber-dojo/start-points-exercises.git \
     https://github.com/cyber-dojo/start-points-custom.git    \
+
+
+
+
+    #https://github.com/cyber-dojo/start-points-exercises.git \
