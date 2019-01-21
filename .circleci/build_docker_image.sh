3d0e66bf963b803780da87a9ad25d87ea742b62f
Jon Jagger <jon@jaggersoft.com>
2019-01-21 11:10:10 +0000

    refactoring; simplify helper script

--- .circleci/build_docker_image.sh
+++ .circleci/build_docker_image.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 readonly SCRIPT=build_cyber_dojo_start_points_image.sh
 readonly IMAGE_NAME=cyberdojo/start-points
 
 # build the FROM image so it won't be docker pulled
 docker build \
   --tag cyberdojo/start-points-base \
-  "${MY_DIR}/.."
+  "${ROOT_DIR}"
 
 # smoke test building an image
-"${MY_DIR}/../${SCRIPT}" \
+"${ROOT_DIR}/${SCRIPT}" \
   ${IMAGE_NAME} \
     https://github.com/cyber-dojo/start-points-languages.git \
     https://github.com/cyber-dojo/start-points-exercises.git \
