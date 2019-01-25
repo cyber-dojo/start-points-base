f0d66f96ae0d455ddd044574f1553ae5629ed4f9
Jon Jagger <jon@jaggersoft.com>
2019-01-25 08:41:34 +0000

    refactoring; working towards adding defaults for --languages/--exercises/--custom

--- build_cyber_dojo_start_points_image.sh
+++ build_cyber_dojo_start_points_image.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+readonly IMAGE_NAME="${1}"
+readonly REPO_NAMES="${*:2}"
+
 show_use()
 {
-  readonly MY_NAME=$(basename "$0")
+  readonly my_name=$(basename "$0")
   cat <<- EOF
 
-  Use: ./${MY_NAME} <image-name> \\
+  Use: ./${my_name} <image-name> \\
     --languages <git-repo-urls> \\
     --exercises <git-repo-urls> \\
     --custom    <git-repo-urls> \\
@@ -16,20 +19,20 @@ show_use()
   It will contain git clones of all the specified repos.
 
   Example
-  \$ ${MY_NAME} acme/a-start-point \\
+  \$ ${my_name} acme/a-start-point \\
     --languages file:///.../asm-assert \\
                 file:///.../java-junit \\
     --exercises file:///.../katas      \\
     --custom    file:///.../yahtzee    \\
 
   Example
-  \$ ${MY_NAME} acme/another-start-point \\
+  \$ ${my_name} acme/another-start-point \\
     --languages https://github.com/.../my-languages.git \\
     --exercises https://github.com/.../my-exercises.git \\
     --custom    https://github.com/.../my-custom.git    \\
 
   Example
-  \$ ${MY_NAME} acme/yet-another \\
+  \$ ${my_name} acme/yet-another \\
     --languages "\$(< my-language-selection.txt)"        \\
     --exercises https://github.com/.../my-exercises.git \\
     --custom    https://github.com/.../my-custom.git    \\
@@ -53,27 +56,27 @@ error()
 
 # - - - - - - - - - - - - - - - - -
 
-readonly IMAGE_NAME="${1}"
-readonly REPO_NAMES="${*:2}"
-
-if [ -z "${IMAGE_NAME}" ] || [ "${IMAGE_NAME}" = '--help' ];  then
-  show_use
-  exit 0
-fi
-if ! hash git 2> /dev/null; then
-  error 1 'git needs to be installed'
-fi
-if ! hash docker 2> /dev/null; then
-  error 2 'docker needs to be installed'
-fi
-if [ -z "${IMAGE_NAME}" ]; then
-  show_use
-  error 3 'missing <image_name>'
-fi
-if [ -z "${REPO_NAMES}" ]; then
-  show_use
-  error 4 'missing <git-repo-urls>'
-fi
+check_arguments()
+{
+  if [ -z "${IMAGE_NAME}" ] || [ "${IMAGE_NAME}" = '--help' ]; then
+    show_use
+    exit 0
+  fi
+  if ! hash git 2> /dev/null; then
+    error 1 'git needs to be installed'
+  fi
+  if ! hash docker 2> /dev/null; then
+    error 2 'docker needs to be installed'
+  fi
+  if [ -z "${IMAGE_NAME}" ]; then
+    show_use
+    error 3 'missing <image_name>'
+  fi
+  if [ -z "${REPO_NAMES}" ]; then
+    show_use
+    error 4 'missing <git-repo-urls>'
+  fi
+}
 
 # - - - - - - - - - - - - - - - - -
 
@@ -81,38 +84,59 @@ fi
 readonly CONTEXT_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-base.XXXXXXXXX)
 cleanup() { rm -rf "${CONTEXT_DIR}" > /dev/null; }
 trap cleanup EXIT
+
+declare seen_languages='false'
+declare seen_exercises='false'
+declare seen_custom='false'
+
+# - - - - - - - - - - - - - - - - -
+
+git_clone_into_context_dir()
+{
+  local type="${1}"
+  local repo_name="${2}"
+  local index="${3}"
+  cd "${CONTEXT_DIR}/${type}"
+  git clone --verbose --depth 1 "${repo_name}" ${index}
+  case "${type}" in
+  languages) seen_languages='true';;
+  exercises) seen_exercises='true';;
+  custom)    seen_custom='true'   ;;
+  esac
+  cd ${index}
+  declare sha
+  sha=$(git rev-parse HEAD)
+  echo "${type} ${index} ${sha} ${repo_name}" >> "${CONTEXT_DIR}/shas.txt"
+}
+
+# - - - - - - - - - - - - - - - - -
+
+check_arguments
+
 mkdir "${CONTEXT_DIR}/languages"
 mkdir "${CONTEXT_DIR}/exercises"
 mkdir "${CONTEXT_DIR}/custom"
 
-declare type=""
+declare type=''
 declare index=0
 for repo_name in $REPO_NAMES; do
     case "${repo_name}" in
-    --languages)
-      type=languages
-      continue
-      ;;
-    --exercises)
-      type=exercises
-      continue
-      ;;
-    --custom)
-      type=custom
-      continue
-      ;;
+    --languages) type=languages; continue;;
+    --exercises) type=exercises; continue;;
+    --custom)    type=custom;    continue;;
     esac
     if [ -z "${type}" ]; then
-      error 5 'missing --languages/--exercises/--custom'
+      error 5 "repo ${repo_name} without preceeding --languages/--exercises/--custom"
     fi
-    cd "${CONTEXT_DIR}/${type}"
-    git clone --verbose --depth 1 "${repo_name}" ${index}
-    cd ${index}
-    declare sha
-    sha=$(git rev-parse HEAD)
-    echo "${type} ${index} ${sha} ${repo_name}" >> "${CONTEXT_DIR}/shas.txt"
+    git_clone_into_context_dir "${type}" "${repo_name}" "${index}"
     index=$((index + 1))
 done
+
+# set defaults
+#echo "seen_languages=${seen_languages}"
+#echo "seen_exercises=${seen_exercises}"
+#echo "seen_custom=${seen_custom}"
+#exit 1
 
 # create a Dockerfile in the temp docker context
 { echo 'FROM cyberdojo/start-points-base';
