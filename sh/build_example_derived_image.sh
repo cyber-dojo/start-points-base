1f8995400642f10005ba25332ec045fa4b5d2a4b
Jon Jagger <jon@jaggersoft.com>
2019-01-30 13:39:09 +0000

    refactoring

--- sh/build_example_derived_image.sh
+++ sh/build_example_derived_image.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 set -e
 
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
-readonly CP_DATA_SET="${ROOT_DIR}/test_data/cp_data_set.sh"
 readonly CUSTOM=${1}
 readonly EXERCISES=${2}
 readonly LANGUAGES=${3}
 
 # - - - - - - - - - - - - - - - - -
 # create tmp dirs
-readonly TMP_BASE=$(mktemp -d)
-cleanup() { rm -rf "${TMP_BASE}" > /dev/null; }
+readonly TMP_DIR=$(mktemp -d)
+cleanup() { rm -rf "${TMP_DIR}" > /dev/null; }
 trap cleanup EXIT
-readonly C_TMP_DIR="${TMP_BASE}/custom"
-readonly E_TMP_DIR="${TMP_BASE}/exercises"
-readonly L_TMP_DIR="${TMP_BASE}/languages"
+readonly C_TMP_DIR="${TMP_DIR}/custom"
+readonly E_TMP_DIR="${TMP_DIR}/exercises"
+readonly L_TMP_DIR="${TMP_DIR}/languages"
 mkdir "${C_TMP_DIR}"
 mkdir "${E_TMP_DIR}"
 mkdir "${L_TMP_DIR}"
 
 # - - - - - - - - - - - - - - - - -
 # create git repos in tmp dirs from named test-data-sets
+readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+readonly CP_DATA_SET="${ROOT_DIR}/test_data/cp_data_set.sh"
+
 "${CP_DATA_SET}" "${C_TMP_DIR}" "${CUSTOM}"
 "${CP_DATA_SET}" "${E_TMP_DIR}" "${EXERCISES}"
 "${CP_DATA_SET}" "${L_TMP_DIR}" "${LANGUAGES}"
