6eb58ceba4a17b6efd3781f59960d7087e40f76f
Jon Jagger <jon@jaggersoft.com>
2019-01-30 16:28:56 +0000

    switch creation of custom start-point git repo to use container

--- sh/build_example_derived_image.sh
+++ sh/build_example_derived_image.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 set -e
 
+readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+
 readonly CUSTOM=${1}
 readonly EXERCISES=${2}
 readonly LANGUAGES=${3}
 
 # - - - - - - - - - - - - - - - - -
 # create tmp dirs
-readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-start-points-base.XXX)
+# Put this off root-dir so it works for DockerToolbox.
+readonly TMP_DIR=$(mktemp -d "${ROOT_DIR}/tmp/cyber-dojo-start-points-base.XXX")
 rm_tmp_dir() { rm -rf "${TMP_DIR}" > /dev/null; }
 trap rm_tmp_dir EXIT
 
 # - - - - - - - - - - - - - - - - -
 # create git repos in tmp dirs from named test-data-sets
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 readonly CP_DATA_SET="${ROOT_DIR}/test_data/cp_data_set.sh"
 
-"${CP_DATA_SET}" "${TMP_DIR}/custom"    "${CUSTOM}"
+#"${CP_DATA_SET}" "${TMP_DIR}/custom"    "${CUSTOM}"
 "${CP_DATA_SET}" "${TMP_DIR}/exercises" "${EXERCISES}"
 "${CP_DATA_SET}" "${TMP_DIR}/languages" "${LANGUAGES}"
 
 # problem here is docker-machine means TMP_DIR will
 # not be visible outside the default VM...
 # I think I need to specify a more local dir
-#docker run \
-#  --rm \
-#  --volume "${TMP_DIR}/custom:${TMP_DIR}/custom:rw" \
-#  cyberdojo/start-points-test-data \
-#    "${TMP_DIR}/custom" \
-#    "${CUSTOM}"
+docker run \
+  --user root \
+  --rm \
+  --tmpfs /tmp \
+  --volume "${TMP_DIR}/custom:/app/tmp/custom:rw" \
+  cyberdojo/create-start-points-test-data \
+    "/app/tmp/custom" \
+    "${CUSTOM}"
 
 # - - - - - - - - - - - - - - - - -
 # build the named image from the git repos in the tmp dirs
--- sh/pipe_build_up.sh
+++ sh/pipe_build_up.sh
@@ -3,5 +3,5 @@ set -e
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 "${SH_DIR}/build_base_docker_image.sh"
 "${SH_DIR}/build_docker_images.sh"
-"${SH_DIR}/build_example_derived_image.sh" custom exercises languages
+"${SH_DIR}/build_example_derived_image.sh" good_custom good_exercises good_languages
 "${SH_DIR}/containers_up.sh"
