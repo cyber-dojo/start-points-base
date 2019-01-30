bdc282c0ed97db10e4cc0e3c5356e292c37bbe73
Jon Jagger <jon@jaggersoft.com>
2019-01-30 16:51:11 +0000

    refactoring; renaming

--- sh/build_example_derived_image.sh
+++ sh/build_example_derived_image.sh
@@ -14,25 +14,21 @@ trap rm_tmp_dir EXIT
 
 create_git_repo_from_named_data_set()
 {
-  local repo_type="${1}"
+  local dir_name="${1}"
   local data_set_name="${2}"
   docker run \
     --user root \
     --rm \
     --tmpfs /tmp \
-    --volume "${TMP_DIR}/${repo_type}:/app/tmp/${repo_type}:rw" \
+    --volume "${TMP_DIR}/${dir_name}:/app/tmp/${dir_name}:rw" \
     cyberdojo/create-start-points-test-data \
-      "/app/tmp/${repo_type}" \
+      "/app/tmp/${dir_name}" \
       "${data_set_name}"
 }
 
-readonly CUSTOM=${1}
-readonly EXERCISES=${2}
-readonly LANGUAGES=${3}
-
-create_git_repo_from_named_data_set custom    "${CUSTOM}"
-create_git_repo_from_named_data_set exercises "${EXERCISES}"
-create_git_repo_from_named_data_set languages "${LANGUAGES}"
+create_git_repo_from_named_data_set cust "${1}"
+create_git_repo_from_named_data_set ex   "${2}"
+create_git_repo_from_named_data_set ltf  "${3}"
 
 # - - - - - - - - - - - - - - - - -
 # build the named image from the git repos in the tmp dirs
@@ -42,8 +38,8 @@ readonly IMAGE_NAME=cyberdojo/start-points-test
 "${ROOT_DIR}/${SCRIPT}"     \
   "${IMAGE_NAME}"           \
     --custom                \
-      "file://${TMP_DIR}/custom" \
+      "file://${TMP_DIR}/cust" \
     --exercises             \
-      "file://${TMP_DIR}/exercises" \
+      "file://${TMP_DIR}/ex" \
     --languages             \
-      "file://${TMP_DIR}/languages" \
+      "file://${TMP_DIR}/ltf" \
