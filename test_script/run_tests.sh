81cae6db374831fc9e0e50fe43b24457a9cf553e
Jon Jagger <jon@jaggersoft.com>
2019-01-31 08:55:42 +0100

    refactoring

--- test_script/run_tests.sh
+++ test_script/run_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# These tests run with fake volume-mounts hooked into the main
-# port_cyber_dojo_storer_to_saver.sh script
-
 readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 readonly glob="${1}"
 
