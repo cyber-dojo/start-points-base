b8805678c3f1fabe256a6fcca7a47ac0bdd41f96
Jon Jagger <jon@jaggersoft.com>
2019-02-01 19:34:44 +0100

    prepare first Ruby-created data-set

--- test_data/make_data_set.rb
+++ test_data/make_data_set.rb
@@ -1,11 +1,19 @@
 #!/usr/bin/ruby
 
 def data_set_name
-  ARGV[1]
+  ARGV[0]
 end
 
 def target_dir
-  ARGV[2]
+  ARGV[1]
 end
 
-puts "Hello from Ruby #{data_set_name}, #{target_dir}"
+def custom_repo_contains_no_manifests
+  `cp -R /app/good_custom #{target_dir}`
+  Dir.glob("#{target_dir}/**/manifest.json").each do |manifest_filename|
+    `rm #{manifest_filename}`
+  end
+end
+
+puts(data_set_name)
+eval(data_set_name)
--- test_script/test_002_container_error_messages.sh
+++ test_script/test_002_container_error_messages.sh
@@ -2,14 +2,26 @@
 readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 . ${my_dir}/starter_helpers.sh
 
-X_test_002a_custom_repo_contains_no_manifests()
+test_002a_custom_repo_contains_no_manifests()
 {
-  local image_name=cyberdojo/dummy
-  #build_start_points_image ${image_name} --custom
+  local image_name="${FUNCNAME[0]}"
+  TMP_DIR=$(make_TMP_DIR_for_git_repos)
+
+  create_git_repo_in_TMP_DIR_from_data_set custom_repo_contains_no_manifests
+  create_git_repo_in_TMP_DIR_from_data_set good_exercises
+  create_git_repo_in_TMP_DIR_from_data_set good_languages
+
+  build_start_points_image \
+    "${image_name}"  \
+      --custom    "file://${TMP_DIR}/custom_repo_contains_no_manifests" \
+      --exercises "file://${TMP_DIR}/good_exercises" \
+      --languages "file://${TMP_DIR}/good_languages"
+
   #assert_stdout_equals ''
   #assert_stderr_equals 'ERROR: --custom requires at least one <git-repo-url>'
   #assert_status_equals 9
-  #refute_image_created "${image_name}"
+  docker image rm "${image_name}"
+  refute_image_created "${image_name}"
 }
 
 . ${my_dir}/shunit2_helpers.sh
