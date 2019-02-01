d8ebccfc44ec896bf60743148b323a2ce24495be
Jon Jagger <jon@jaggersoft.com>
2019-02-01 13:13:46 +0100

    refactoring

--- sh/build_example_derived_image.sh
+++ sh/build_example_derived_image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+
 create_git_repo_from_named_data_set()
 {
   local tmp_dir="${1}"
@@ -15,35 +17,60 @@ create_git_repo_from_named_data_set()
 }
 
 # - - - - - - - - - - - - - - - - -
-# create tmp dirs
-# This off root-dir (and not /tmp say) so it works in Docker-Toolbox.
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
-if [ ! -d "${ROOT_DIR}/tmp" ]; then
-  mkdir "${ROOT_DIR}/tmp"
-fi
-readonly TMP_DIR=$(mktemp -d "${ROOT_DIR}/tmp/cyber-dojo-start-points-base.XXX")
-rm_tmp_dir()
+
+using_DockerToolbox()
 {
-  echo "inside rm_tmp_dir"
-  echo "TMP_DIR=:${TMP_DIR}:"
-  whoami
-  pwd
-  ls -al "${TMP_DIR}"
+  if [ -n "${DOCKER_MACHINE_NAME}" ]; then
+    true
+  else
+    false
+  fi
+}
+
+# - - - - - - - - - - - - - - - - -
+
+declare TMP_DIR=''
+
+make_TMP_DIR()
+{
+  if using_DockerToolbox; then
+    [ -d "${ROOT_DIR}/tmp" ] || mkdir "${ROOT_DIR}/tmp"
+    TMP_DIR=$(mktemp -d "${ROOT_DIR}/tmp/cyber-dojo-start-points-base.XXX")
+  else
+    TMP_DIR=$(mktemp -d)
+  fi
+}
+
+remove_TMP_DIR()
+{
   rm -rf "${TMP_DIR}" > /dev/null;
 }
-trap rm_tmp_dir EXIT
+trap remove_TMP_DIR EXIT
 
+# - - - - - - - - - - - - - - - - -
+
+make_TMP_DIR
 create_git_repo_from_named_data_set "${TMP_DIR}" good_custom
 create_git_repo_from_named_data_set "${TMP_DIR}" good_exercises
 create_git_repo_from_named_data_set "${TMP_DIR}" good_languages
 
 # - - - - - - - - - - - - - - - - -
-# build the named image from the git repos in the tmp dirs
-readonly SCRIPT=build_cyber_dojo_start_points_image.sh
-readonly IMAGE_NAME=cyberdojo/start-points-test
 
-"${ROOT_DIR}/${SCRIPT}"     \
-  "${IMAGE_NAME}"           \
+build_image_script_name()
+{
+  echo 'build_cyber_dojo_start_points_image.sh'
+}
+
+image_name()
+{
+  echo 'cyberdojo/start-points-test'
+}
+
+# - - - - - - - - - - - - - - - - -
+# build the named image from the git repos in the tmp dirs
+
+"${ROOT_DIR}/$(build_image_script_name)"     \
+  "$(image_name)"           \
     --custom                \
       "file://${TMP_DIR}/good_custom" \
     --exercises             \
