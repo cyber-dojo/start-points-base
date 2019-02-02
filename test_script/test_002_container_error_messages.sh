d5665e1bb3925ba9253190116f1af2f37405cc77
Jon Jagger <jon@jaggersoft.com>
2019-02-02 20:03:37 +0000

    refactoring; add tests for single and multiples of each repo type

--- test_script/test_002_container_error_messages.sh
+++ test_script/test_002_container_error_messages.sh
@@ -6,9 +6,7 @@ test_002a_custom_repo_contains_no_manifests()
 {
   make_TMP_DIR_for_git_repos
   local C1_TMP_DIR=$(create_git_repo_in_TMP_DIR_from custom-tennis)
-  local C2_TMP_DIR=$(create_git_repo_in_TMP_DIR_from custom-yahtzee)
   local E1_TMP_DIR=$(create_git_repo_in_TMP_DIR_from exercises-bowling-game)
-  local E2_TMP_DIR=$(create_git_repo_in_TMP_DIR_from exercises-tiny-maze)
   local L1_TMP_DIR=$(create_git_repo_in_TMP_DIR_from ltf-python-unittest)
   local L2_TMP_DIR=$(create_git_repo_in_TMP_DIR_from ltf_no_manifests)
 
@@ -17,19 +15,15 @@ test_002a_custom_repo_contains_no_manifests()
     "${image_name}"            \
       --custom                 \
         "file://${C1_TMP_DIR}" \
-        "file://${C2_TMP_DIR}" \
       --exercises              \
         "file://${E1_TMP_DIR}" \
-        "file://${E2_TMP_DIR}" \
       --languages              \
         "file://${L1_TMP_DIR}" \
         "file://${L2_TMP_DIR}"
 
   refute_image_created
   assert_stdout_includes $(echo -e "--custom \t file://${C1_TMP_DIR}")
-  assert_stdout_includes $(echo -e "--custom \t file://${C2_TMP_DIR}")
   assert_stdout_includes $(echo -e "--exercises \t file://${E1_TMP_DIR}")
-  assert_stdout_includes $(echo -e "--exercises \t file://${E2_TMP_DIR}")
   assert_stdout_includes $(echo -e "--languages \t file://${L1_TMP_DIR}")
   assert_stdout_includes $(echo -e "--languages \t file://${L2_TMP_DIR}")
   #assert_stdout_line_count_equals 6
--- test_script/test_003_success_cases.sh
+++ test_script/test_003_success_cases.sh
@@ -2,7 +2,36 @@
 readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 . ${my_dir}/starter_helpers.sh
 
-test_003a_simple_success_case()
+test_003a_single_repo_for_each_category()
+{
+  make_TMP_DIR_for_git_repos
+  local C1_TMP_DIR=$(create_git_repo_in_TMP_DIR_from custom-tennis)
+  local E1_TMP_DIR=$(create_git_repo_in_TMP_DIR_from exercises-bowling-game)
+  local L1_TMP_DIR=$(create_git_repo_in_TMP_DIR_from ltf-csharp-nunit)
+
+  local image_name="${FUNCNAME[0]}"
+  build_start_points_image     \
+    "${image_name}"            \
+      --custom                 \
+        "file://${C1_TMP_DIR}" \
+      --exercises              \
+        "file://${E1_TMP_DIR}" \
+      --languages              \
+        "file://${L1_TMP_DIR}"
+
+  assert_image_created
+  assert_stdout_includes $(echo -e "--custom \t file://${C1_TMP_DIR}")
+  assert_stdout_includes $(echo -e "--exercises \t file://${E1_TMP_DIR}")
+  assert_stdout_includes $(echo -e "--languages \t file://${L1_TMP_DIR}")
+  assert_stdout_line_count_equals 3
+
+  assert_stderr_equals ''
+  assert_status_equals 0
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+test_003b_more_than_one_repo_for_each_category()
 {
   make_TMP_DIR_for_git_repos
   local C1_TMP_DIR=$(create_git_repo_in_TMP_DIR_from custom-tennis)
