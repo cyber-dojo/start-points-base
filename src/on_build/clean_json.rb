70f728f2e2c28a4cd8ce7e16af4b9c4e4d6ad3a5
Jon Jagger <jon@jaggersoft.com>
2019-02-10 09:52:18 +0000

    refactoring; collapse duplication

---
+++ src/on_build/clean_json.rb
@@ -0,0 +1,34 @@
+require_relative 'json_duplicate_keys'
+require 'json'
+
+module CleanJson
+
+  include JsonDuplicateKeys
+
+  def clean_json(url, filename)
+    content = IO.read(filename)
+    json = parse_json(url, filename, content)
+    duplicates = json_duplicate_keys(content)
+    if duplicates == {}
+      json
+    else
+      msg = json_pretty_duplicate_keys(duplicates)
+      STDERR.puts('ERROR: duplicate keys in manifest.json file')
+      STDERR.puts("--#{@type} #{url}")
+      STDERR.puts("filename='#{filename}'")
+      STDERR.puts(msg)
+      exit(18)
+    end
+  end
+
+  def parse_json(url, filename, content)
+    JSON.parse!(content)
+  rescue JSON::ParserError => error
+    STDERR.puts('ERROR: bad JSON in manifest.json file')
+    STDERR.puts("--#{@type} #{url}")
+    STDERR.puts("filename='#{filename}'")
+    STDERR.puts(error)
+    exit(17)
+  end
+
+end
--- src/on_build/language_manifest_checker.rb
+++ src/on_build/language_manifest_checker.rb
@@ -1,6 +1,5 @@
 require_relative 'read_manifest_filenames'
-require_relative 'json_duplicate_keys'
-require 'json'
+require_relative 'clean_json'
 
 class LanguageManifestChecker
 
@@ -17,39 +16,13 @@ class LanguageManifestChecker
 
   private
 
-  include JsonDuplicateKeys
+  include CleanJson
 
   def check_one(url, filenames)
     filenames.each do |filename|
       json = clean_json(url, filename)
       #...
     end
-  end
-
-  def clean_json(url, filename)
-    content = IO.read(filename)
-    json = parse_json(url, filename, content)
-    duplicates = json_duplicate_keys(content)
-    if duplicates == {}
-      json
-    else
-      msg = json_pretty_duplicate_keys(duplicates)
-      STDERR.puts('ERROR: duplicate keys in manifest.json file')
-      STDERR.puts("--#{@type} #{url}")
-      STDERR.puts("filename='#{filename}'")
-      STDERR.puts(msg)
-      exit(18)
-    end
-  end
-
-  def parse_json(url, filename, content)
-    JSON.parse!(content)
-  rescue JSON::ParserError => error
-    STDERR.puts('ERROR: bad JSON in manifest.json file')
-    STDERR.puts("--#{@type} #{url}")
-    STDERR.puts("filename='#{filename}'")
-    STDERR.puts(error)
-    exit(17)
   end
 
 end
