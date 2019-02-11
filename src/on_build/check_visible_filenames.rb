761579c48f531fe40b03fd0a8f05b48134e19b2d
Jon Jagger <jon@jaggersoft.com>
2019-02-11 10:42:17 +0000

    refactoring; extract methods

--- src/on_build/check_visible_filenames.rb
+++ src/on_build/check_visible_filenames.rb
@@ -6,50 +6,72 @@ module CheckVisibleFilenames
 
   def check_visible_filenames(url, manifest_filename, json)
     visible_filenames = json['visible_filenames']
+    exit_unless_visible_filenames_is_array(visible_filenames, url, manifest_filename)
+    exit_if_visible_filenames_is_empty(visible_filenames, url, manifest_filename)
+    visible_filenames.each_with_index do |filename,index|
+      exit_unless_visible_filename_is_a_String(visible_filenames, filename, index, url, manifest_filename)
+      exit_if_visible_filename_is_empty(visible_filenames, filename, index, url, manifest_filename)
+      exit_unless_visible_filename_is_portable(visible_filenames, filename, index, url, manifest_filename)
+    end
+    # TODO:30.check no duplicate visible files
+    # TODO:31.check all visible files exist and are world-readable
+    # TODO:32.check cyber-dojo.sh is a visible_filename
+
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
+  def exit_unless_visible_filenames_is_array(visible_filenames, url, manifest_filename)
     unless visible_filenames.is_a?(Array)
       title = 'visible_filenames is not an Array'
       msg = "\"visible_filenames\": #{visible_filenames}"
       show_error(title, url, manifest_filename, msg)
       exit(25)
     end
+  end
+
+  def exit_if_visible_filenames_is_empty(visible_filenames, url, manifest_filename)
     if visible_filenames.empty?
       title = 'visible_filenames is empty'
       msg = "\"visible_filenames\": #{visible_filenames}"
       show_error(title, url, manifest_filename, msg)
       exit(26)
     end
-    visible_filenames.each_with_index do |filename,index|
-      unless filename.is_a?(String)
-        title = "visible_filenames[#{index}] is not a String"
+  end
+
+  def exit_unless_visible_filename_is_a_String(visible_filenames, filename, index, url, manifest_filename)
+    unless filename.is_a?(String)
+      title = "visible_filenames[#{index}] is not a String"
+      msg = "\"visible_filenames\": #{visible_filenames}"
+      show_error(title, url, manifest_filename, msg)
+      exit(27)
+    end
+  end
+
+  def exit_if_visible_filename_is_empty(visible_filenames, filename, index, url, manifest_filename)
+    if filename.empty?
+      title = "visible_filenames[#{index}] is empty"
+      msg = "\"visible_filenames\": #{visible_filenames}"
+      show_error(title, url, manifest_filename, msg)
+      exit(28)
+    end
+  end
+
+  def exit_unless_visible_filename_is_portable(visible_filenames, filename, index, url, manifest_filename)
+    filename.each_char do |ch|
+      unless portable?(ch)
+        title = "visible_filenames[#{index}] has non-portable character '#{ch}'"
         msg = "\"visible_filenames\": #{visible_filenames}"
         show_error(title, url, manifest_filename, msg)
-        exit(27)
-      end
-      if filename.empty?
-        title = "visible_filenames[#{index}] is empty"
-        msg = "\"visible_filenames\": #{visible_filenames}"
-        show_error(title, url, manifest_filename, msg)
-        exit(28)
-      end
-      filename.each_char do |ch|
-        unless portable?(ch)
-          title = "visible_filenames[#{index}] has non-portable character '#{ch}'"
-          msg = "\"visible_filenames\": #{visible_filenames}"
-          show_error(title, url, manifest_filename, msg)
-          exit(29)
-        end
-      end
-      if filename[0] == '-'
-        title = "visible_filenames[#{index}] has non-portable leading character '-'"
-        msg = "\"visible_filenames\": #{visible_filenames}"
-        show_error(title, url, manifest_filename, msg)
-        exit(30)
+        exit(29)
       end
     end
-    # TODO:30.check no duplicate visible files
-    # TODO:31.check all visible files exist and are world-readable
-    # TODO:32.check cyber-dojo.sh is a visible_filename
-
+    if filename[0] == '-'
+      title = "visible_filenames[#{index}] has non-portable leading character '-'"
+      msg = "\"visible_filenames\": #{visible_filenames}"
+      show_error(title, url, manifest_filename, msg)
+      exit(30)
+    end
   end
 
   def portable?(ch)
