082882ca8785fb3f27d7eaf4161d41360472a568
Jon Jagger <jon@jaggersoft.com>
2019-02-13 09:19:20 +0000

    refactoring; extract methods

--- src/on_build/check_hidden_filenames.rb
+++ src/on_build/check_hidden_filenames.rb
@@ -5,28 +5,39 @@ module CheckHiddenFilenames
   include ShowError
 
   def check_hidden_filenames(url, manifest_filename, json)
-    unless json.has_key?('hidden_filenames')
-      return
+    if json.has_key?('hidden_filenames')
+      hidden_filenames = json['hidden_filenames']
+      exit_if_hidden_filenames_malformed(hidden_filenames, url, manifest_filename, json)
+      exit_if_hidden_filenames_bad_regexp(hidden_filenames, url, manifest_filename, json)
+      exit_if_hidden_filenames_has_duplicates(hidden_filenames, url, manifest_filename, json)
     end
-    hidden_filenames = json['hidden_filenames']
-    unless hidden_filenames.is_a?(Array)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - -
+
+  def exit_if_hidden_filenames_malformed(hidden_filenames, url, manifest_filename, json)
+    if hidden_filenames_malformed?(hidden_filenames)
       title = 'hidden_filenames must be an Array of Strings'
       msg = "\"hidden_filenames\": #{hidden_filenames}"
       show_error(title, url, manifest_filename, msg)
       exit(39)
+    end
+  end
+
+  def hidden_filenames_malformed?(hidden_filenames)
+    unless hidden_filenames.is_a?(Array)
+      return true
     end
     unless hidden_filenames.all?{|s| s.is_a?(String) }
-      title = 'hidden_filenames must be an Array of Strings'
-      msg = "\"hidden_filenames\": #{hidden_filenames}"
-      show_error(title, url, manifest_filename, msg)
-      exit(39)
+      return true
     end
     if hidden_filenames.any?{|s| s == '' }
-      title = 'hidden_filenames must be an Array of Strings'
-      msg = "\"hidden_filenames\": #{hidden_filenames}"
-      show_error(title, url, manifest_filename, msg)
-      exit(39)
+      return true
     end
+    false
+  end
+
+  def exit_if_hidden_filenames_bad_regexp(hidden_filenames, url, manifest_filename, json)
     hidden_filenames.each_with_index do |s,index|
       begin
         Regexp.new(s)
@@ -37,6 +48,9 @@ module CheckHiddenFilenames
         exit(40)
       end
     end
+  end
+
+  def exit_if_hidden_filenames_has_duplicates(hidden_filenames, url, manifest_filename, json)
     hidden_filenames.each do |filename|
       dup_indexes = get_dup_indexes(hidden_filenames, filename)
       unless dup_indexes == ''
@@ -46,7 +60,6 @@ module CheckHiddenFilenames
         exit(41)
       end
     end
-
   end
 
 end
