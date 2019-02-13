edebe2bd72bdf728b272b307d10d6b9ebc8c10e2
Jon Jagger <jon@jaggersoft.com>
2019-02-13 11:10:04 +0000

    refactoring; extract method

--- src/on_build/check_tab_size.rb
+++ src/on_build/check_tab_size.rb
@@ -7,27 +7,33 @@ module CheckTabSize
   def check_tab_size(url, manifest_filename, json)
     if json.has_key?('tab_size')
       tab_size = json['tab_size']
-      unless tab_size.is_a?(Integer)
-        title = 'tab_size must be an Integer'
-        if tab_size.is_a?(String)
-          msg = "\"tab_size\": \"#{tab_size}\""
-        else
-          msg = "\"tab_size\": #{tab_size}"
-        end
-        show_error(title, url, manifest_filename, msg)
-        exit(42)
-      end
-
-      unless (1..8).include?(tab_size)
-        title = 'tab_size must be an Integer (1..8)'
-        msg = "\"tab_size\": #{tab_size}"
-        show_error(title, url, manifest_filename, msg)
-        exit(43)
-      end
+      exit_unless_tab_size_is_integer(tab_size, url, manifest_filename, json)
+      exit_unless_tab_size_in_range(tab_size, url, manifest_filename, json)
     end
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - -
 
+  def exit_unless_tab_size_is_integer(tab_size, url, manifest_filename, json)
+    unless tab_size.is_a?(Integer)
+      title = 'tab_size must be an Integer'
+      if tab_size.is_a?(String)
+        msg = "\"tab_size\": \"#{tab_size}\""
+      else
+        msg = "\"tab_size\": #{tab_size}"
+      end
+      show_error(title, url, manifest_filename, msg)
+      exit(42)
+    end
+  end
+
+  def exit_unless_tab_size_in_range(tab_size, url, manifest_filename, json)
+    unless (1..8).include?(tab_size)
+      title = 'tab_size must be an Integer (1..8)'
+      msg = "\"tab_size\": #{tab_size}"
+      show_error(title, url, manifest_filename, msg)
+      exit(43)
+    end    
+  end
 
 end
