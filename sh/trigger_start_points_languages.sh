32a0a9736d95f8c20cae1f82a4a75dfeb5e47241
Jon Jagger <jon@jaggersoft.com>
2019-02-18 18:23:55 +0000

    added notes to build-trigger script

--- sh/trigger_start_points_languages.sh
+++ sh/trigger_start_points_languages.sh
@@ -3,21 +3,40 @@ set -ev
 
 # This script runs in .circleci/config.yml after
 # a cyberdojo/start-points-base docker image is
-# successfully created.
-# Originally instead of a git commit+push the .yml file did a
+# successfully created and pushed to dockerhub.
+# Originally it did this:
 #
-# curl --fail -X POST https://circleci.com/api/v1.1/project/github/cyber-dojo/start-points-languages/build?circle-token=${CIRCLECI_BUILD_TOKEN}
+# curl --fail -X POST \
+#  https://circleci.com/api/v1.1/project/github/cyber-dojo/start-points-languages/build?circle-token=${CIRCLECI_BUILD_TOKEN}
 #
 # The problems with this approach are
-# o) when the trigger reached start-points-languages .circleci/config.yml
+# o) when the trigger reached start-points-language's .circleci/config.yml
 #    its Helm commands did not see a SHA change, so did not see an
 #    image-tag change, and so did not do a deployment.
 # o) Since the SHA did not change, the image was being built
 #    with the same tag multiple times. Not good practice.
 #
-# To avoid these problems we now do a regular git commit+push
-# using a "machine-user". See
-# https://circleci.com/docs/2.0/gh-bb-integration/#creating-a-machine-user
+# To avoid these problems we now do a [git commit] + [git push]
+# using a "machine-user" called cyber-dojo-machine-user.
+# See https://circleci.com/docs/2.0/gh-bb-integration/#creating-a-machine-user
+#
+# There are two git-repos/circleci-projects involved...
+# 1) cyber-dojo/start-points-base
+# 2) cyber-dojo/start-points-languages
+# The ./circleci/config.yml for 1) has to do a [git commit] + [git push]
+# into the github repo for 2)
+#
+# The cyber-dojo-machine-user had to be added as a collaborator
+# (with Admin rights) to the github repo for 1).
+# Only then did CircleCI gave me the option to automatically
+# generate and add the cyber-dojo-machine-user's SSH keys to the CircleCI
+# start-points-base project in the CircleCI cyber-dojo organization.
+# Note too that these SSH keys are the preferred SSH keys for this project
+# and so do not have to be explicitly set in the config.yml file.
+#
+# The cyber-dojo-machine-user had to be added as a collaborator
+# (with Write rights) to the github repo for 2).
+# This is so the [git push] works.
 
 cd /tmp
 git clone https://github.com/cyber-dojo/start-points-languages.git
