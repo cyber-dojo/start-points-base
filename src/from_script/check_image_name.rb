91c984e996cbb0f913af95cada6a70e800f3485a
Jon Jagger <jon@jaggersoft.com>
2019-04-17 15:54:23 +0100

    refactoring; towards multiple error messages (image_name)

--- src/from_script/check_image_name.rb
+++ src/from_script/check_image_name.rb
@@ -7,20 +7,33 @@ module CheckImageName
   include WellFormedImageName
 
   def check_image_name(url, filename, json, error_code)
+    ok = image_name_is_string(url, filename, json, error_code)
+    ok && image_name_is_well_formed(url, filename, json, error_code)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
+  def image_name_is_string(url, filename, json, error_code)
+    result = true
     image_name = json['image_name']
     unless image_name.is_a?(String)
       title = 'image_name is not a String'
       key = quoted('image_name')
       msg = "#{key}: #{image_name}"
-      show_error(title, url, filename, msg)
-      exit(error_code)
+      result = error(title, url, filename, msg, error_code)
     end
+    result
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
+  def image_name_is_well_formed(url, filename, json, error_code)
+    image_name = json['image_name']
     unless well_formed_image_name?(image_name)
       title = 'image_name is malformed'
       key = quoted('image_name')
       msg = "#{key}: #{quoted(image_name)}"
-      show_error(title, url, filename, msg)
-      exit(error_code)
+      error(title, url, filename, msg, error_code)
     end
   end
 
