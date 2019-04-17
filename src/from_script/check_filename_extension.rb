f7ab87b872f4692d200426b51cd7fcf1cc9cb3c5
Jon Jagger <jon@jaggersoft.com>
2019-04-17 16:12:33 +0100

    refactoring; towards multiple error messages (filename_extension)

--- src/from_script/check_filename_extension.rb
+++ src/from_script/check_filename_extension.rb
@@ -5,27 +5,25 @@ module CheckFilenameExtension
   include ShowError
 
   def check_filename_extension(url, manifest_filename, json, error_code)
-    filename_extension = json['filename_extension']
-    if filename_extension.is_a?(String)
-      filename_extension = [ filename_extension ]
-    end
-    exit_unless_filename_extension_is_well_formed(filename_extension, url, manifest_filename, error_code)
-    exit_if_filename_extension_is_empty_string(filename_extension, url, manifest_filename, error_code)
-    exit_if_filename_extension_dotless(filename_extension, url, manifest_filename, error_code)
-    exit_if_filename_extension_only_dots(filename_extension, url, manifest_filename, error_code)
-    exit_if_filename_extension_duplicates(filename_extension, url, manifest_filename, error_code)
+    ok = check_filename_extension_well_formed(url, manifest_filename, json, error_code)
+    ok = ok && check_filename_extension_empty_string(url, manifest_filename, json, error_code)
+    ok && check_filename_extension_dotless(url, manifest_filename, json, error_code)
+    ok && check_filename_extension_only_dots(url, manifest_filename, json, error_code)
+    ok && check_filename_extension_duplicates(url, manifest_filename, json, error_code)
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  def exit_unless_filename_extension_is_well_formed(filename_extension, url, manifest_filename, error_code)
+  def check_filename_extension_well_formed(url, manifest_filename, json, error_code)
+    result = true
+    filename_extension = get_filename_extension(json)
     unless filename_extension_is_well_formed?(filename_extension)
       title = 'filename_extension must be a String or Array of Strings'
       key = quoted('filename_extension')
       msg = "#{key}: #{filename_extension}"
-      show_error(title, url, manifest_filename, msg)
-      exit(error_code)
+      result = error(title, url, manifest_filename, msg, error_code)
     end
+    result
   end
 
   def filename_extension_is_well_formed?(filename_extension)
@@ -34,52 +32,64 @@ module CheckFilenameExtension
         filename_extension.all?{|e| e.is_a?(String) }
   end
 
-  def exit_if_filename_extension_is_empty_string(filename_extension, url, manifest_filename, error_code)
+  def check_filename_extension_empty_string(url, manifest_filename, json, error_code)
+    result = true
+    filename_extension = get_filename_extension(json)
     filename_extension.each_with_index do |ext, index|
       if ext == ''
         title = "filename_extension[#{index}]='' cannot be empty String"
         key = quoted('filename_extension')
         msg = "#{key}: #{filename_extension}"
-        show_error(title, url, manifest_filename, msg)
-        exit(error_code)
+        result = error(title, url, manifest_filename, msg, error_code)
       end
     end
+    result
   end
 
-  def exit_if_filename_extension_dotless(filename_extension, url, manifest_filename, error_code)
+  def check_filename_extension_dotless(url, manifest_filename, json, error_code)
+    filename_extension = get_filename_extension(json)
     filename_extension.each_with_index do |ext, index|
       unless ext[0] == '.'
         title = "filename_extension[#{index}]=#{quoted(ext)} must start with a dot"
         key = quoted('filename_extension')
         msg = "#{key}: #{filename_extension}"
-        show_error(title, url, manifest_filename, msg)
-        exit(error_code)
+        error(title, url, manifest_filename, msg, error_code)
       end
     end
   end
 
-  def exit_if_filename_extension_only_dots(filename_extension, url, manifest_filename, error_code)
+  def check_filename_extension_only_dots(url, manifest_filename, json, error_code)
+    filename_extension = get_filename_extension(json)
     filename_extension.each_with_index do |ext, index|
       if ext == '.'
         title = "filename_extension[#{index}]=#{quoted(ext)} must be more than just a dot"
         key = quoted('filename_extension')
         msg = "#{key}: #{filename_extension}"
-        show_error(title, url, manifest_filename, msg)
-        exit(error_code)
+        error(title, url, manifest_filename, msg, error_code)
       end
     end
   end
 
-  def exit_if_filename_extension_duplicates(filename_extension, url, manifest_filename, error_code)
+  def check_filename_extension_duplicates(url, manifest_filename, json, error_code)
+    filename_extension = get_filename_extension(json)
     filename_extension.each do |ext|
       dups = get_dup_indexes(filename_extension, ext)
       unless dups == []
         title = "filename_extension#{dups} are duplicates of #{quoted(ext)}"
         key = quoted('filename_extension')
         msg = "#{key}: #{filename_extension}"
-        show_error(title, url, manifest_filename, msg)
-        exit(error_code)
+        error(title, url, manifest_filename, msg, error_code)
+        return # don't produce same error twice
       end
+    end
+  end
+
+  def get_filename_extension(json)
+    filename_extension = json['filename_extension']
+    if filename_extension.is_a?(String)
+      [ filename_extension ]
+    else
+      filename_extension
     end
   end
 
