1fe580e37a095a9c86ac9ef5283b0e317349484f
Jon Jagger <jon@jaggersoft.com>
2019-04-20 19:16:54 +0100

    rename one of the error() functions into ba_error()

--- src/from_script/bad_args.rb
+++ src/from_script/bad_args.rb
@@ -30,11 +30,11 @@ def exit_non_zero_if_duplicate_urls_for(status, type)
     urls.select{|url| urls.count(url) != 1}.uniq.each do |dup|
       msg += "#{dup}\n"
     end
-    error(status, msg)
+    ba_error(status, msg)
   end
 end
 
-def error(status, message)
+def ba_error(status, message)
   STDERR.puts("ERROR: #{message}")
   exit(status)
 end
@@ -42,20 +42,20 @@ end
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 case image_name
-when '--custom'    then error(4, '--custom requires preceding <image_name>')
-when '--exercises' then error(5, '--exercises requires preceding <image_name>')
-when '--languages' then error(6, '--languages requires preceding <image_name>')
+when '--custom'    then ba_error(4, '--custom requires preceding <image_name>')
+when '--exercises' then ba_error(5, '--exercises requires preceding <image_name>')
+when '--languages' then ba_error(6, '--languages requires preceding <image_name>')
 end
 
 unless ['--custom','--exercises','--languages'].include?(image_type)
-  error(7, "<image-name> must be followed by one of --custom/--exercises/--languages")
+  ba_error(7, "<image-name> must be followed by one of --custom/--exercises/--languages")
 end
 
 if git_repo_urls == []
   case image_type
-  when '--custom'    then error(8, '--custom requires at least one <git-repo-url>')
-  when '--exercises' then error(9, '--exercises requires at least one <git-repo-url>')
-  when '--languages' then error(10, '--languages requires at least one <git-repo-url>')
+  when '--custom'    then ba_error(8, '--custom requires at least one <git-repo-url>')
+  when '--exercises' then ba_error(9, '--exercises requires at least one <git-repo-url>')
+  when '--languages' then ba_error(10, '--languages requires at least one <git-repo-url>')
   end
 end
 
@@ -64,7 +64,7 @@ exit_non_zero_if_duplicate_urls_for(12, '--exercises')
 exit_non_zero_if_duplicate_urls_for(13, '--languages')
 
 unless well_formed_image_name?(image_name)
-  error(14, "malformed <image-name> #{image_name}")
+  ba_error(14, "malformed <image-name> #{image_name}")
 end
 
 exit(0)
