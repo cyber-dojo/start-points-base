65e34e4fbc8feadb1916f03b1ffe29215874903b
Jon Jagger <jon@jaggersoft.com>
2019-06-22 14:21:08 +0100

    reinstate commented out test asserts

--- test_script/starter_helpers.sh
+++ test_script/starter_helpers.sh
@@ -178,9 +178,9 @@ assert_stdout_equals_use()
   local -r help_line_3="$ ./cyber-dojo start-point create <name> --exercises <url>..."
   local -r help_line_4="$ ./cyber-dojo start-point create <name> --languages <url>..."
   assert_stdout_includes "${help_line_1}"
-  #assert_stdout_includes "${help_line_2}"
-  #assert_stdout_includes "${help_line_3}"
-  #assert_stdout_includes "${help_line_4}"
+  assert_stdout_includes "${help_line_2}"
+  assert_stdout_includes "${help_line_3}"
+  assert_stdout_includes "${help_line_4}"
   assert_stdout_line_count_equals 54
 }
 
