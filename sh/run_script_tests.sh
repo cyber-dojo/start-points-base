e2678b9e3f6bd9196ba13849c9a93de961f87224
JonJagger <jon@jaggersoft.com>
2019-12-01 15:03:43 +0000

    Pull starter-base image before running script tests

--- sh/run_script_tests.sh
+++ sh/run_script_tests.sh
@@ -3,4 +3,9 @@ set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
+# pull starter-base image so its stdout/stderr messages
+# do not interfere with test assertions
+export $(docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env')
+docker pull cyberdojo/starter-base:${CYBER_DOJO_STARTER_BASE_TAG}
+
 "${MY_DIR}/../test_script/run_tests.sh" ${*}
