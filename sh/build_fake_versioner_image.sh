6c6cae780bec692d74837107c25fa4a606cf603e
Jon Jagger <jon@jaggersoft.com>
2019-12-30 22:57:18 +0000

    Do assert_equals on fake cyberdojo/versioner:latest image

--- sh/build_fake_versioner_image.sh
+++ sh/build_fake_versioner_image.sh
@@ -6,23 +6,49 @@ readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 build_fake_versioner()
 {
   local env_vars="${1}"
-  local -r sha="$(git_commit_sha)"
-  local -r tag="${sha:0:7}"
-  local -r fake_sha="CYBER_DOJO_START_POINTS_BASE_SHA=${sha}"
-  local -r fake_tag="CYBER_DOJO_START_POINTS_BASE_TAG=${tag}"
+  local -r fake_sha="$(git_commit_sha)"
+  local -r fake_tag="${fake_sha:0:7}"
   local -r fake=fake_versioner
-  docker rm --force ${fake} > /dev/null 2>&1 | true
-  docker run --detach --name ${fake} alpine:latest sh -c 'mkdir /app' > /dev/null
+
+  docker rm --force "${fake}" > /dev/null 2>&1 | true
+  docker run                  \
+    --detach                  \
+    --env RELEASE=999.999.999 \
+    --env SHA="${sha}"        \
+    --name "${fake}"          \
+    alpine:latest             \
+      sh -c 'mkdir /app' > /dev/null
+
+  # replace START_POINTS_BASE env-vars with fakes
+  env_vars=$(echo "${env_vars}" | grep --invert-match CYBER_DOJO_START_POINTS_BASE_SHA)
+  env_vars=$(echo "${env_vars}" | grep --invert-match CYBER_DOJO_START_POINTS_BASE_TAG)
   echo "${env_vars}" >  /tmp/.env
-  echo "${fake_sha}" >> /tmp/.env # replaces earlier entry when exported
-  echo "${fake_tag}" >> /tmp/.env # replaces earlier entry when exported
-  docker cp /tmp/.env ${fake}:/app/.env
-  docker commit ${fake} cyberdojo/versioner:latest > /dev/null 2>&1
-  docker rm --force ${fake} > /dev/null 2>&1
-  # show it
-  docker run --rm -it cyberdojo/versioner:latest sh -c 'cat /app/.env' | tail -n -2
-  echo "${fake_sha}:"
-  echo "${fake_tag}:"
+  echo "CYBER_DOJO_START_POINTS_BASE_SHA=${fake_sha}" >> /tmp/.env
+  echo "CYBER_DOJO_START_POINTS_BASE_TAG=${fake_tag}" >> /tmp/.env
+  docker cp /tmp/.env "${fake}:/app/.env"
+  docker commit "${fake}" cyberdojo/versioner:latest > /dev/null 2>&1
+  docker rm --force "${fake}" > /dev/null 2>&1
+  # check it
+  expected="CYBER_DOJO_START_POINTS_BASE_SHA=${fake_sha}"
+  actual=$(docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env' | grep CYBER_DOJO_START_POINTS_BASE_SHA)
+  assert_equal "${expected}" "${actual}"
+
+  expected="CYBER_DOJO_START_POINTS_BASE_TAG=${fake_tag}"
+  actual=$(docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env' | grep CYBER_DOJO_START_POINTS_BASE_TAG)
+  assert_equal "${expected}" "${actual}"
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+assert_equal()
+{
+  local -r expected="${1}"
+  local -r actual="${2}"
+  if [ "${expected}" != "${actual}" ]; then
+    echo "ERROR"
+    echo "expected:${expected}"
+    echo "  actual:${actual}"
+    exit 42
+  fi
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
