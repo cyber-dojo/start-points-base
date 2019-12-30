992da589bef51253263945d0c71f027eacb47092
Jon Jagger <jon@jaggersoft.com>
2019-12-30 10:17:24 +0000

    Drop line-number from expected stder message - to brittle

--- test_script/run_tests.sh
+++ test_script/run_tests.sh
@@ -3,7 +3,7 @@ set -e
 
 readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
-# Set CYBER_DOJO_STARTER_BASE_TAG so we tunnel through
+# Set CYBER_DOJO_START_POINTS_BASE_TAG so we tunnel through
 # cyberdojo -> cyber-dojo-inner -> cat-start-point-create.sh
 readonly BASE_SHA=$(cd "${my_dir}" && cd .. && git rev-parse HEAD)
 readonly TAG=${BASE_SHA:0:7}
