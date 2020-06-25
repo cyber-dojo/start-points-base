63871418626fe411d8a9ac75913ccff1c444cd59
Jon Jagger <jon@jaggersoft.com>
2020-06-25 07:49:01 +0100

    Add tests for [start-point create ... tagged-url]

--- test_script/test_success_cases.sh
+++ test_script/test_success_cases.sh
@@ -34,6 +34,39 @@ test_more_than_one_git_repo()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+test_one_git_repo_with_tagged_url()
+{
+  local -r image_name="${FUNCNAME[0]}"
+  local -r L_URL=4963697@https://github.com/cyber-dojo-start-points/gcc-assert
+
+  build_start_points_image       \
+    "${image_name}"              \
+      --languages "${L_URL}"
+
+  assert_image_created
+  assert_stderr_equals ''
+  assert_status_equals 0
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+test_more_than_one_git_repo_with_tagged_urls()
+{
+  local -r image_name="${FUNCNAME[0]}"
+  local -r L1_URL=ce71235@https://github.com/cyber-dojo-start-points/python-unittest
+  local -r L2_URL=f618605@https://github.com/cyber-dojo-start-points/ruby-minitest
+
+  build_start_points_image \
+    "${image_name}"        \
+      --languages          \
+        "${L1_URL}"    \
+        "${L2_URL}"
+
+  assert_image_created
+  assert_stderr_equals ''
+  assert_status_equals 0
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 echo "::${0##*/}"
 readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 . ${my_dir}/starter_helpers.sh
