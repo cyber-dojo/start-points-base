a6678cb644349d417d276754dd3d1582e2eb9f92
JonJagger <jon@kosli.com>
2024-08-01 14:42:24 +0100

    Dockerfile: update base image to move past curl vulnerabilities

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -74,7 +74,7 @@ clean_top_5()
   # FROM cyberdojo/docker-base:0b9a792
 
   local -r L1="Puma starting in single mode..."
-  local -r L2='* Puma version: 6.4.2 (ruby 3.3.3-p89) ("The Eagle of Durango")'
+  local -r L2='* Puma version: 6.4.2 (ruby 3.3.4-p94) ("The Eagle of Durango")'
   local -r L3="*  Min threads: 0"
   local -r L4="*  Max threads: 5"
   local -r L5="*  Environment: production"
