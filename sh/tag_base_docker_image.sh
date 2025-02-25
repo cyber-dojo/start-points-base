1890fabd43224243eb98df1cdcad97aec7977aea
JonJagger <jon@kosli.com>
2025-02-25 16:28:26 +0000

    Switch to non-reusable workflow

--- sh/tag_base_docker_image.sh
+++ sh/tag_base_docker_image.sh
@@ -4,12 +4,6 @@ set -Eeu
 tag_base_docker_image()
 {
   docker tag "$(image_name):latest" "$(image_name):$(image_tag)"
-  if on_ci; then
-    echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
-  fi
   docker push "$(image_name):$(image_tag)"
-  if on_ci; then
-    docker logout
-  fi
 }
 
