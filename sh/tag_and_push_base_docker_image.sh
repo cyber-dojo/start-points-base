5c2a3427b3c4c6244c3e1a5cc66cbc1c6a26934f
JonJagger <jon@kosli.com>
2025-05-11 15:02:27 +0100

    Try not doing the docker-push

--- sh/tag_and_push_base_docker_image.sh
+++ sh/tag_and_push_base_docker_image.sh
@@ -7,6 +7,6 @@ tag_and_push_base_docker_image()
   docker tag "$(image_name):latest" "$(image_name):$(image_tag)"
   # The built image must be pushed to dockerhub because it is used the FROM image
   # in the created dynamically Dockerfile in a `cyber-dojo start-point create` command.
-  docker push "$(image_name):$(image_tag)"
+  # docker push "$(image_name):$(image_tag)"
 }
 
